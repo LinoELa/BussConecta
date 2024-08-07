c0f147a8f1f6e9b5a74badf865a4c7460821683f
LinoEla <jamelarry22@gmail.com>
2024-08-07 19:40:21 +0200

    RENDER: busqueda de archivos estaticos

--- BusConecta/settings.py
+++ BusConecta/settings.py
@@ -136,17 +136,39 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # STATIC_ROOT = 'static'
 
+# if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+#     # and renames the files with unique names for each version to support long-term caching
+#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATIC_ROOT = 'static/'
+# Ruta base del proyecto
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# URL y directorio para archivos estáticos
+STATIC_URL = '/static/'
+
+
+# # Directorios adicionales donde Django también buscará archivos estáticos
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+STATIC_URL = '/static/'
+# This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # # Directorios adicionales donde Django también buscará archivos estáticos
+    STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'),]
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# STATIC_ROOT = 'static/'
 
 
 # Default primary key field type
